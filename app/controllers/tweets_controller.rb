0ec47de06c4ee7ba5e9abf34b19db1844abc2a33
i-shintaro <shintaroiwashita7857@gmail.com>
2024-09-16 07:34:06 +0900

    firstcommit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,9 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show ,:search]
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC")
+    #インスタンス = モデル名.order("並び替えの基準となるカラム名 並び順")
   end
 
   def new
@@ -8,11 +11,49 @@ class TweetsController < ApplicationController
   end
 
   def create
+    binding.pry
     Tweet.create(tweet_params)
     redirect_to '/'
   end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
+  end
+
     private
     def tweet_params
-      params.require(:tweet).permit(:name, :image, :text)
+      params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
     end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create,]
+  resources :tweets do
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
+  resources :users, only: :show
+
 end
